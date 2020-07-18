a598ee8163e68dae6939aa0d0f4151c8c4921aab
Ryslan271 <57673799+Ryslan271@users.noreply.github.com>
2020-07-18 12:11:24 +0300

    Add files via upload

---
+++ bot_ct/models/users.py
@@ -0,0 +1,19 @@
+import sqlalchemy as sa
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'user'
+
+    id = sa.Column(sa.Integer,
+                   primary_key=True, autoincrement=True)
+    question = sa.Column(sa.String, nullable=True)
+    answer = sa.Column(sa.String, nullable=True)
+
+
+class Questions(SqlAlchemyBase):
+    __tablename__ = 'questions'
+
+    id = sa.Column(sa.Integer,
+                   primary_key=True, autoincrement=True)
+    question = sa.Column(sa.String, nullable=True)
